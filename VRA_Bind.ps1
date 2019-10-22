897c7d6544575346542c0487beae08d8a8d542ae
stillnocake <56047361+stillnocake@users.noreply.github.com>
2019-10-22 10:03:30 -0400

    initial commit

---
+++ VRA_Bind.ps1
@@ -0,0 +1,19 @@
+$server = Read-Host "Input server name: "
+$user = ".\administrator"
+#$pswd = ""
+$pswd = Read-host -AsSecureString "Input local admin password: "
+$creds = New-Object System.Management.Automation.PSCredential $user,$pswd
+
+$configVRA = @{
+    connect = ""
+    line1 = "C:\VRMGuestAgent\WinService.exe -u;shutdown -r -t 00 -f"
+    line2 = "C:\VRMGuestAgent\WinService.exe -i -h SERVER -p ssl"
+    restart = "Restart-Computer -Wait"
+}
+
+$cred = Get-Credential
+Connect-VIServer $configVRA.connect -Credential $cred
+
+Invoke-VMScript -VM $server -ScriptType Bat -ScriptText $configVRA.line1 -GuestUser $creds
+Restart-Computer -ComputerName $server -Wait -For PowerShell -Delay 2
+Invoke-VMScript -VM $server -ScriptType Bat -ScriptText $configVRA.line2 -GuestUser $creds
